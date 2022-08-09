458b1f852c27f1ff7181a00a0379d137a3c907dc
overkidding <46853321+overkidding@users.noreply.github.com>
2022-08-09 23:17:58 +0200

    Added worldguard custom flags

--- src/main/java/me/overkidding/chunkbuster/ChunkBuster.java
+++ src/main/java/me/overkidding/chunkbuster/ChunkBuster.java
@@ -1,5 +1,10 @@
 package me.overkidding.chunkbuster;
 
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import lombok.Getter;
 import me.overkidding.chunkbuster.command.ChunkBusterCommand;
 import me.overkidding.chunkbuster.listener.ChunkBusterListener;
@@ -14,7 +19,26 @@ public class ChunkBuster extends JavaPlugin {
 
     private final WorkloadRunnable workloadRunnable = new WorkloadRunnable();
 
+    // declare your flag as a field accessible to other parts of your code (so you can use this to check it)
+// note: if you want to use a different type of flag, make sure you change StateFlag here and below to that type
+    public static StateFlag CHUNK_BUSTING_FLAG;
 
+    @Override
+    public void onLoad() {
+        FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
+        try {
+            StateFlag flag = new StateFlag("chunk-busting", true);
+            registry.register(flag);
+            CHUNK_BUSTING_FLAG = flag;
+        } catch (FlagConflictException e) {
+            Flag<?> existing = registry.get("chunk-busting");
+            if (existing instanceof StateFlag) {
+                CHUNK_BUSTING_FLAG = (StateFlag) existing;
+            } else {
+                this.getPluginLoader().disablePlugin(this);
+            }
+        }
+    }
 
     @Override
     public void onEnable() {
--- src/main/java/me/overkidding/chunkbuster/listener/ChunkBusterListener.java
+++ src/main/java/me/overkidding/chunkbuster/listener/ChunkBusterListener.java
@@ -1,11 +1,19 @@
 package me.overkidding.chunkbuster.listener;
 
+import com.sk89q.worldedit.util.Location;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import com.sk89q.worldguard.protection.regions.RegionQuery;
 import me.overkidding.chunkbuster.ChunkBuster;
 import me.overkidding.chunkbuster.utils.XItemStack;
 import me.overkidding.chunkbuster.workload.filler.impl.DistributedFiller;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -34,10 +42,21 @@ public class ChunkBusterListener implements Listener {
         ItemStack chunkBusterItem = XItemStack.deserialize(configuration.getConfigurationSection("ITEM"), s -> ChatColor.translateAlternateColorCodes('&', s));
 
         if(chunkBusterItem.isSimilar(item)){
-            if(event.getClickedBlock() == null){
+            Block clickedBlock = event.getClickedBlock();
+            if(clickedBlock == null){
                 player.sendMessage(ChatColor.RED + "You need to click on a block to let chunk buster do his work!");
                 return;
             }
+            LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player);
+            RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+            RegionQuery query = container.createQuery();
+            World world = localPlayer.getWorld();
+            Location loc = new Location(world, clickedBlock.getX(), clickedBlock.getY(), clickedBlock.getZ());
+            if(!query.testState(loc, localPlayer, ChunkBuster.CHUNK_BUSTING_FLAG)){
+                player.sendMessage(ChatColor.RED + "You can't chunk bust a protected region!");
+                return;
+            }
+
             long timeLeft = cooldowns.getOrDefault(player.getUniqueId(), -1L) - System.currentTimeMillis();
             if(timeLeft > 0){
                 long secondsLeft = TimeUnit.MILLISECONDS.toSeconds(timeLeft);
@@ -55,7 +74,7 @@ public class ChunkBusterListener implements Listener {
             ItemStack handItem = player.getItemInHand();
             handItem.setAmount(handItem.getAmount() - 1);
             player.setItemInHand(handItem);
-            Chunk chunk = event.getClickedBlock().getChunk();
+            Chunk chunk = clickedBlock.getChunk();
             new DistributedFiller(ChunkBuster.getInstance().getWorkloadRunnable()).fill(chunk, Material.AIR);
             player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                     configuration.getString("SETTINGS.MESSAGES.START")
